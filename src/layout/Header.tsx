8b3f2046599c7064fcff619a6e5c7928acb305aa
Legenda <schurik458@gmail.com>
2024-09-02 17:23:56 +0300

    Fixed icon name

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,8 +2,8 @@ import React, {memo} from 'react';
 import {Box, ButtonGroup, IconButton, Stack, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 import AuthenticationButton from "components/AuthenticationButton";
-import shoppingCart from 'assets/icon/shopping-cart.svg';
-import heartIcon from 'assets/icon/heart-icon.svg';
+import shoppingCart from 'assets/icon/shopping_cart.svg';
+import heartIcon from 'assets/icon/heart_icon.svg';
 import SearchField from "components/SearchField";
 
 function Header() {
